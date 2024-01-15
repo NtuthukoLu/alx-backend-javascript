9ecf8d8078c2ffafea5b8601a484d1ae2e5a6ba2
Ntuthuko <ntuthukolu@gmail.com>
2024-01-15 14:43:55 +0200

    task 9

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${this.income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${this.income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
