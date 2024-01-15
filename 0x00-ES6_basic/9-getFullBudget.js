8550f383a614ecc4f85bae9699c3cb9b49986175
Ntuthuko <ntuthukolu@gmail.com>
2024-01-15 14:49:05 +0200

    task 9

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -3,15 +3,9 @@ import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
-  const fullBudget = {
+  return {
     ...budget,
-    getIncomeInDollars(income) {
-      return `$${this.income}`;
-    },
-    getIncomeInEuros(income) {
-      return `${this.income} euros`;
-    },
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} euros`,
   };
-
-  return fullBudget;
 }
