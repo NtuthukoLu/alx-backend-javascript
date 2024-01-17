42d5791f18267f344c3a415741761a6f14ad36c8
Ntuthuko <ntuthukolu@gmail.com>
2024-01-17 17:13:24 +0200

    eslint

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
-export default function handleResponseFromAPI(promise){
-    return promise
-    .then(() => ({stause: 200, body: 'success'}))
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ stause: 200, body: 'success' }))
     .catch(() => Error())
     .finally(() => console.log('Got a response from the API'));
 }
