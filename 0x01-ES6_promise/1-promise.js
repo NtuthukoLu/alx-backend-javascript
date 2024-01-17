a1a0bb74f2b04de85869ab528fe18f67bccc1edd
Ntuthuko <ntuthukolu@gmail.com>
2024-01-17 16:05:05 +0200

    task 1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,10 @@
+export default function getFullResponseFromAPI(success) {
+  const promise = new Promise(((resolve, reject) => {
+    if (success === true) {
+      resolve({ Status: 200, body: 'Success' });
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  }));
+  return promise;
+}
