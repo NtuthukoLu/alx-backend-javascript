c7370ed200182051fef51e1d54898855c88c914c
Ntuthuko <ntuthukolu@gmail.com>
2024-01-24 20:05:26 +0200

    task 8

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,14 +1,7 @@
 export default function divideFunction(numerator, denominator) {
-  try {
-    if (denominator === 0) {
-      throw new Error('cannot divide by 0');
-    }
-
-    const results = numerator / denominator;
-    return results;
-  } catch (error) {
-    console.error(`(Error: ${error.message})`);
-  } finally {
-    console.log('Division completed');
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
   }
+
+  return numerator / denominator;
 }
