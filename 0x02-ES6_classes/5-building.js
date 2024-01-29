3be789085ef6a919520900cc7d7891c599fabd27
Ntuthuko <ntuthukolu@gmail.com>
2024-01-29 20:10:55 +0200

    elinst

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,25 +1,17 @@
-export default class Building
-{
-
-    constructor(sqft)
-    {
-        if (this.constructor !== Building && !this.evacuationWarningMessage) 
-        {
-            throw new Error('Class extending Building must override evacuationWarningMessage');
-        }
-        this._sqft = sqft;
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
+    this._sqft = sqft;
+  }
 
-    get sqft()
-    {
-        return (this._sqft);
-    }
-    set sqft(value)
-    {
-        this._sqft = value;
-        return (value);
-    }
+  get sqft() {
+    return (this._sqft);
+  }
 
-   
-
+  set sqft(value) {
+    this._sqft = value;
+    return (value);
+  }
 }
