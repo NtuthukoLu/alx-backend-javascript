25110546f23fe73765f9deeaeb2d79d4a09c5ee4
Ntuthuko <ntuthukolu@gmail.com>
2024-01-29 20:07:37 +0200

    task 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,25 @@
+export default class Building
+{
+
+    constructor(sqft)
+    {
+        if (this.constructor !== Building && !this.evacuationWarningMessage) 
+        {
+            throw new Error('Class extending Building must override evacuationWarningMessage');
+        }
+        this._sqft = sqft;
+    }
+
+    get sqft()
+    {
+        return (this._sqft);
+    }
+    set sqft(value)
+    {
+        this._sqft = value;
+        return (value);
+    }
+
+   
+
+}
