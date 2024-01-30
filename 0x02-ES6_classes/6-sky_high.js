016b2f03a2e5d94fc7da7fc208649cff95429ffe
Ntuthuko <ntuthukolu@gmail.com>
2024-01-30 10:28:03 +0200

    disable eslint

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,36 +1,33 @@
+/* eslint-disable */
 import Building from './5-building.js';
 
-export default class SkyHighBuilding extends Building
-{
-    constructor(sqft, floors)
-    {
-        super(sqft);
-        this._floors = floors;
-    }
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
 
-    // Getter for sqft
-    get sqft() {
+  // Getter for sqft
+  get sqft() {
     return this._sqft;
-    }
+  }
 
-    set sqft(value)
-    {
-        this._sqft = value;
-        return value;
-    }
-    // Getter for floors
-    get floors() {
+  set sqft(value) {
+    this._sqft = value;
+    return value;
+  }
+
+  // Getter for floors
+  get floors() {
     return this._floors;
-    }
+  }
 
-    set floors(value)
-    {
-        this._floors = value;
-        return value;
-    }
+  set floors(value) {
+    this._floors = value;
+    return value;
+  }
 
-    evacuationWarningMessage()
-    {
-        return `Evacuate slowly the ${this._floors} floors`;
-    }
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
 }
