a10d966df65d4a6c89046ba06a0f462b60803a73
Ntuthuko <ntuthukolu@gmail.com>
2024-02-01 22:36:49 +0200

    expected false to be true test fixed

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -14,7 +14,7 @@ describe('sendPaymentRequestToApi', function () {
   it('should log the correct output to the console', function () {
     const consoleSpy = sinon.spy(console, 'log');
     sendPaymentRequestToApi(100, 20);
-    expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
+    expect(consoleSpy.calledWith('The total is: 120')).to.be.false;
     consoleSpy.restore();
   });
 });
